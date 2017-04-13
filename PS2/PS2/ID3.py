b6da66d2b2412437789dac1e2d5acc6f189de7df
jholm117 <jholm117@gmail.com>
2017-04-13 14:41:52 -0500

    here you go theo

--- PS2/PS2/ID3.py
+++ PS2/PS2/ID3.py
@@ -8,23 +8,25 @@ def ID3(examples, default):
   and the target class variable is a special attribute with the name "Class".
   Any missing attributes are denoted with a value of "?"
   '''
+
+  
 #if examples empty 
 	if not examples:
 		return default
 
 	#set up refernces to first congressman
 	firstPerson = examples[0]
-	affiliation = firstPerson[Class]
+	affiliation = firstPerson["Class"]
 	AllSameClass = True
 	
 	AllSameAttVector = True	
 	AttributeVector = firstPerson.values()
-	AttributeVector.remove(firstPerson[Class])
+	AttributeVector.remove(firstPerson["Class"])
 	
 	for congressman in examples:
 		
 		#if affiliation of current iteration is different from last
-		if(AllSameClass and congressman[Class] != affiliation):
+		if(AllSameClass and congressman["Class"] != affiliation):
 			AllSameClass = False
 			
 		#if attribute vectors have all been the same so far
@@ -32,19 +34,24 @@ def ID3(examples, default):
 			
 			#attribute vector is dict.values without Class value
 			currentAttributeVector = congressman.values()
-			currentAttributeVector.remove(congressman[Class])
+			currentAttributeVector.remove(congressman["Class"])
 
 			#if attribute vector differs			
 			if(AttributeVector != currentAttributeVector):				
 				AllSameAttVector = False
-				
-			#else increase appropriate counters
-			else:
-				if(congressman[Class] == "democrat":
-					demos+=1
-				else:
-					repubs+=1		
+			
+		# increase appropriate counters	
+		if(congressman["Class"] == "democrat":
+			demos+=1
+		else:
+			repubs+=1		
 		
+	#MODE
+	if(demos >= repubs):
+			MODE = "demos"
+		else:
+			MODE = "republican"
+	
 #if examples all the same classification return it			
 	if(AllSameClass):
 		return affiliation
@@ -52,12 +59,30 @@ def ID3(examples, default):
 	
 #if all examples have the same attribute vector return most common class
 	elif(AllSameAttVector):
-		if(demos >= repubs)
-			return "democrat"
-		else
-			return "republican"
-	
- 
+		return MODE
+			
+	else:
+		best = ChooseAttribute()
+		
+		#decision tree with best as root
+		tree = Node()
+		
+		indieVariables = ["y","n","?"]
+		for v in indieVariables :
+			#elements of examples with best = v
+			examples1 = []
+			for congressman in examples:
+			
+				#if the congressman had the same response as v add them to examples1
+				if (congressman[best] == v):
+					examples1.append(congressman)
+			
+			subtree = ID3(examples1, MODE)
+			
+			# add sub tree to root?
+			tree.children[v] = subtree
+			
+		return tree
 
 def prune(node, examples):
   '''
