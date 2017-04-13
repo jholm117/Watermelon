b43fd5082b9088337e3c0920b87bb813c5612f01
theo <theo.bisdikian@gmail.com>
2017-04-13 18:41:03 -0500

    returns default node on empty examples

--- PS2/PS2/ID3.py
+++ PS2/PS2/ID3.py
@@ -11,7 +11,9 @@ def ID3(examples, default):
 
 	#if examples empty 
 	if not examples:
-		return default
+		ret = Node()
+		ret.label = default
+		return ret
 
 	#set up refernces to first example
 	firstPerson = examples[0]
